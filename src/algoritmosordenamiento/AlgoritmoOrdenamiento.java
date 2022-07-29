cf277f9bb3b35c0c0e7d2a10394141e867b1ce1f
Freyder Ortega <fmoz1994@hotmail.com>
2022-07-29 14:01:19 -0500

    se modifica el arrgle original

--- src/algoritmosordenamiento/AlgoritmoOrdenamiento.java
+++ src/algoritmosordenamiento/AlgoritmoOrdenamiento.java
@@ -3,30 +3,28 @@ package algoritmosordenamiento;
 public class AlgoritmoOrdenamiento {
 
     public static void main(String[] args) {
-        int arreglo[] = {21, 40, 4, 9, 10, 35};
+        int arreglo[] = {40, 21, 4, 9, 10, 35};
         int temporal = 0;
-
+       //  System.out.println("      Arreglo original");
+         
+//         for (int i = 0; i < arreglo.length; i++) {
+//             System.out.print(" "+arreglo[i]);
+//            
+//        }
+//         System.out.println("");
         for (int i = 0; i < arreglo.length; i++) {
 
             for (int j = 0; j < arreglo.length - 1; j++) {
 
                 //  System.out.println(" Se cambia el " + arreglo[j] + " por el " + arreglo[j + 1]);
                 if (arreglo[j] > arreglo[j + 1]) {
-
+                  
                     for (int k = 0; k < arreglo.length; k++) {
-////                      if (i == 0 && j == 0) {
-////                            System.out.print(" " + arreglo[k]);
-////                      }else{
-//                      break;
-                        //                   }
-                        for (int h = 0; h < arreglo.length - 1; h++) {
-
-                        }
 
                         System.out.print(" " + arreglo[k]);
                     }
 
-                    System.out.println(" <-- Se cambia el " + arreglo[j] + " por el " + arreglo[j + 1]);
+                    System.out.println(" <-- Se cambia el " + arreglo[j] + " por el " + arreglo[j+1]);
 
                     System.out.println("");
                     temporal = arreglo[j];
