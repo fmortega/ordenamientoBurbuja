583869be5be6dd20147fadd81ed36df0d87690cf
Freyder Ortega <fmoz1994@hotmail.com>
2022-07-29 09:22:03 -0500

    se modifica

--- src/algoritmosordenamiento/AlgoritmoOrdenamiento.java
+++ src/algoritmosordenamiento/AlgoritmoOrdenamiento.java
@@ -4,31 +4,37 @@ public class AlgoritmoOrdenamiento {
 
     public static void main(String[] args) {
         int arreglo[] = {21, 40, 4, 9, 10, 35};
-        int temporal = 1;
-        int contador = 0;
-        int contador2 = 0;
+        int temporal = 0;
+
         for (int i = 0; i < arreglo.length; i++) {
+
             for (int j = 0; j < arreglo.length - 1; j++) {
 
-                System.out.println(" Se cambia el " + arreglo[j] + " por el " + arreglo[j + 1] );
-                
-                 System.out.println("");
-
+                //  System.out.println(" Se cambia el " + arreglo[j] + " por el " + arreglo[j + 1]);
                 if (arreglo[j] > arreglo[j + 1]) {
 
+                    for (int k = 0; k < arreglo.length; k++) {
+////                      if (i == 0 && j == 0) {
+////                            System.out.print(" " + arreglo[k]);
+////                      }else{
+//                      break;
+                        //                   }
+                        for (int h = 0; h < arreglo.length - 1; h++) {
+
+                        }
+
+                        System.out.print(" " + arreglo[k]);
+                    }
+
+                    System.out.println(" <-- Se cambia el " + arreglo[j] + " por el " + arreglo[j + 1]);
+
+                    System.out.println("");
                     temporal = arreglo[j];
 
                     arreglo[j] = arreglo[j + 1];
 
                     arreglo[j + 1] = temporal;
-                    
-                    for (int k = 0; k < arreglo.length; k++) {
-                    for (int h = 0; h < arreglo.length ; h++) {
 
-                    }
-                
-                }
- 
                 }
 
             }
@@ -36,10 +42,8 @@ public class AlgoritmoOrdenamiento {
         }
 
         System.out.println(" ");
-    
 
-
-        System.out.println("Arreglo ordenado");
+        System.out.println("           Arreglo ordenado");
         for (int i = 0; i < arreglo.length; i++) {
             for (int j = 0; j < arreglo.length; j++) {
 
@@ -47,11 +51,10 @@ public class AlgoritmoOrdenamiento {
             if (i + 1 < arreglo.length) {
                 System.out.print(arreglo[i] + " --> ");
             } else {
-                System.out.print(arreglo[i]+" ");
+                System.out.print(arreglo[i] + " ");
             }
 
         }
-    
 
-}
+    }
 }
